e8d40ea9c48c33184de52919a033a3ea776a40c4
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-11-19 12:56:02 +0300

    add gallery section content

--- js/scripts.js
+++ js/scripts.js
@@ -30,3 +30,11 @@ $(document).ready(function() {
     });
 });
 
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+}
+
